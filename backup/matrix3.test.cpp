f8e5154534aae4d228446bbed9aed07d296afb46
Hugues <147343254+hchardin3@users.noreply.github.com>
2024-08-22 19:14:44 +0200

    Delete backup directory

--- backup/matrix3.test.cpp
+++
@@ -1,9 +0,0 @@
-TEST(Matrix3, Constructor_AllElementsEqualToOne) {
-    T values[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
-    Matrix3<T> matrix(values);
-
-    for (int i = 0; i < 9; ++i) {
-        EXPECT_EQ(matrix.data[i], 1);
-    }
-    EXPECT_EQ(matrix.det, 0);
-}
